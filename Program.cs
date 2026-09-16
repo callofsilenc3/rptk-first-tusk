fdcfecf87bb662c7d3aef8b8493e0ccc38534fcb
callofsilenc3 <kalitovanton69@gmail.com>
2026-09-16 21:16:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,11 @@ using System.Threading.Tasks;
 
 class Program
 {
-    // Семафор: 3 разрешения = 3 компьютера
     static SemaphoreSlim computers = new SemaphoreSlim(3, 3);
     static Random random = new Random();
 
     static async Task Main()
     {
-        // Создаём 7 "студентов" (задач)
         Task[] students = new Task[7];
         
         for (int i = 0; i < students.Length; i++)
@@ -19,7 +17,6 @@ class Program
             students[i] = Task.Run(() => DoTask(studentId));
         }
 
-        // Ждём завершения задач
         await Task.WhenAll(students);
         
         Console.WriteLine("\nВсе студенты выполнили задания.");
@@ -29,7 +26,6 @@ class Program
     {
         Console.WriteLine($"Студент {studentId} ждёт компьютер...");
 
-        // Пытаемся занять разрешение (компьютер)
         await computers.WaitAsync();
 
         try
@@ -37,14 +33,12 @@ class Program
             Console.WriteLine($"--> Студент {studentId} сел за компьютер. " +
                               $"Свободно: {computers.CurrentCount}");
             
-            // Имитация работы за пк (1-3 секунды)
             await Task.Delay(random.Next(1000, 3000));
             
             Console.WriteLine($"<-- Студент {studentId} закончил работу.");
         }
         finally
         {
-            // освободить семафор в finally
             computers.Release();
         }
     }
