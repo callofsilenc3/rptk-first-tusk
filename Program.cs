c2d3b12b83c18c4b15fc72a9752c8f44535e2c09
callofsilenc3 <kalitovanton69@gmail.com>
2026-09-17 10:20:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,44 +5,41 @@ using System.Threading.Tasks;
 class Program
 {
     static SemaphoreSlim sem = new SemaphoreSlim(3, 3);
-    static Random rand = new Random();
+    static Random rnd = new Random();
 
     static async Task Main()
     {
-        Console.WriteLine("Студенты идут к компьютерам");
-        Console.WriteLine();
+        Console.WriteLine("Начало");
+        Console.WriteLine("---");
 
         Task[] mas = new Task[7];
+        int count = 0;
 
         for (int i = 0; i < 7; i++)
         {
             int n = i + 1;
             mas[i] = Task.Run(() => Work(n));
+            count++;
         }
 
         await Task.WhenAll(mas);
-        Console.WriteLine();
-        Console.WriteLine("Все закончили");
+
+        Console.WriteLine("---");
+        Console.WriteLine("Конец");
     }
 
     static async Task Work(int n)
     {
-        Console.WriteLine("Студент " + n + " ждет...");
+        Console.WriteLine("Студент " + n + " ждет");
 
         await sem.WaitAsync();
 
-        try
-        {
-            Console.WriteLine("Студент " + n + " сел. Свободно: " + sem.CurrentCount);
-            
-            int t = rand.Next(1000, 3000);
-            await Task.Delay(t);
-            
-            Console.WriteLine("Студент " + n + " освободил компьютер");
-        }
-        finally
-        {
-            sem.Release();
-        }
+        Console.WriteLine("Студент " + n + " сел за комп, свободно: " + sem.CurrentCount);
+
+        int t = rnd.Next(1000, 3000);
+        await Task.Delay(t);
+
+        Console.WriteLine("Студент " + n + " закончил");
+        sem.Release();
     }
 }
